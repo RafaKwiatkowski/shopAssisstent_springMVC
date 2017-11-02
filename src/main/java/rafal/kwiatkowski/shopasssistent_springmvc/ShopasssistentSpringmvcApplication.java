b464f9cd56ec09fdb72539bd0e19edd2b8c96ede
Rafał Kwiatkowski <raf.kwiatkowski93@gmail.com>
2017-11-02 21:26:07 +0100

    initial commit

---
+++ src/main/java/rafal/kwiatkowski/shopasssistent_springmvc/ShopasssistentSpringmvcApplication.java
@@ -0,0 +1,12 @@
+package rafal.kwiatkowski.shopasssistent_springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopasssistentSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopasssistentSpringmvcApplication.class, args);
+	}
+}
---
+++ src/test/java/rafal/kwiatkowski/shopasssistent_springmvc/ShopasssistentSpringmvcApplicationTests.java
@@ -0,0 +1,16 @@
+package rafal.kwiatkowski.shopasssistent_springmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopasssistentSpringmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
